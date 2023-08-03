9367c75baf86eac9a4e9042b2107e0e78227ed1e
dalianmao <34263177+hxyz1118@users.noreply.github.com>
2023-08-03 14:26:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://github.com/sundaqiang/openwrt-packages package/app
+git clone -b 18.06 https://github.com/riverscn/luci-app-omcproxy package/app/luci-app-omcproxy
